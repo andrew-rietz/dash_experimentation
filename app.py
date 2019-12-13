830f11df34b734c756b136496fe3d5e67025bb08
Andrew Rietz <andrew.rietz@gmail.com>
2019-12-12 23:30:07 -0700

    Exploring dash features

--- app.py
+++ app.py
@@ -1,25 +1,17 @@
 import pandas as pd
 import dash
-from dash.dependencies import Input, Output, State
+import io
+import base64
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+import dash_table
 import plotly.graph_objects as go
+from dash.dependencies import Input, Output, State
 
 
 # Create app
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-
-# Load Data
-df = pd.read_csv("./data/power_and_oat.csv")
-
-def create_dropdown_options(df):
-    ls = list(df.columns)
-    options = [{"label": str(col_name), "value": str(col_name)} for col_name in ls]
-    return ls, options
-
-x_filter_list, x_filter_options = create_dropdown_options(df)
-y_filter_list, y_filter_options = create_dropdown_options(df)
 
 # Create app layout
 app.layout = html.Div(
@@ -29,33 +21,132 @@ app.layout = html.Div(
                 html.H4("Sample Meter Data",)
             ]),
         html.Div([
+            dcc.Upload(
+                id="user_upload",
+                children=html.Div([
+                    "Drag and Drop CSV or",
+                    html.A("Select CSV Files")
+                ]),
+                style={
+                    "width": "100%",
+                    "height": "60px",
+                    "lineHeight": "60px",
+                    "borderWidth": "1px",
+                    "borderStyle": "dashed",
+                    "borderRadius": "5px",
+                    "textAlign": "center",
+                    "margin": "10px"
+                },
+                # Allow multiple files to be uploaded
+                # multiple=True
+            ),
+        ]),
+        html.Div(id="output-data-table"),
+        html.Div([
             html.H6("Select Variable for X-Axis"),
-            dcc.Dropdown(
+            dcc.RadioItems(
                 id="x_axis",
-                value="OAT",
+                value="",
                 className="dcc_control",
-                multi=False,
-                options=x_filter_options
+                # options=x_filter_options,
+                inputStyle={
+                    "margin-left": "10px",
+                    "margin-right": "2px",
+                }
             ),
             html.H6("Select Variable for Y-Axis"),
-            dcc.Dropdown(
+            dcc.RadioItems(
                 id="y_axis",
-                value="kW",
+                value="",
                 className="dcc_control",
-                multi=False,
-                options=y_filter_options
+                # options=y_filter_options,
+                inputStyle={
+                    "margin-left": "10px",
+                    "margin-right": "2px",
+                }                
             ),
             dcc.Graph(id="main_graph")
         ])
     ]
 )
 
-# Callback for main map plot
+df = pd.DataFrame()
+
+
+def create_dropdown_options(df):
+    ls = list(df.columns)
+    options = [{"label": str(col_name), "value": str(col_name)} for col_name in ls]
+    return ls, options
+
+
+def parse_csv_contents(file_content, filename):
+    content_type, content_string = file_content.split(",")
+    decoded = base64.b64decode(content_string)
+    return pd.read_csv(io.StringIO(decoded.decode("utf-8")))
+    # try:
+    #     if ".csv" in filename:
+    #         df = pd.read_csv(
+    #             io.StringIO(decoded.decode("utf-8"))
+    #         )
+    #     else:
+    #         return "The provided file must be in CSV format"
+    # except Exception as e:
+    #     print(e)
+    #     return html.Div([
+    #         "There was an error processing this file."
+    #     ])
+    # return df
+
+
+@app.callback(Output("output-data-table", "children"),
+            #   [Input("user_upload", "file_content")],
+            #   [State("user_upload", "filename")]
+)
+def load_table(df, columns, filename):
+    return [
+        html.H5(filename),
+        dash_table.DataTable(
+            data=df.to_dict("records"),
+            columns=columns,
+            editable=True,
+            filter_action="native",
+            row_selectable="multi",
+            style_as_list_view=True,
+        )
+    ]
+
+
+# Callback for data table display
+@app.callback([Output("x_axis", "options"),
+               Output("y_axis", "options")],
+              [Input("user_upload", "contents")],
+              [State("user_upload", "filename")]
+)
+def update_output(contents, filename):
+    if contents is None:
+        return [], []
+    
+    df = parse_csv_contents(contents, filename)
+    columns = [{"name": c_name, "id": c_name}
+               for c_name in df.columns]
+    columns["deletable"] = True
+    columns["renamable"] = True
+
+    x_filter_list, x_filter_options = create_dropdown_options(df)
+    y_filter_list, y_filter_options = create_dropdown_options(df)
+
+    load_table(df, columns, filename)
+
+    
+    return x_filter_options, y_filter_options
+
+
+# Callback for main plot
 @app.callback( # reference elements by id
     [Output("main_graph", "figure")],
     [Input("x_axis", "value"), Input("y_axis", "value")]
 )
-def make_main_figure(x_axis, y_axis):
+def make_main_figure(df, x_axis, y_axis):
     data = go.Scatter(
         x=df[x_axis],
         y=df[y_axis],
